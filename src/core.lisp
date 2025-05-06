c98be48d0f779972f868cfb0f819f57076c0f4f5
FHaggs <pedrofamh@protonmail.com>
2025-05-06 10:44:15 -0300

    Add define-turing-machine macro

--- src/core.lisp
+++ src/core.lisp
@@ -40,44 +40,62 @@
 ;; Key: (current-state current-symbol)
 ;; Value: (new-symbol direction next-state)
 ;; ((current-state symbol) (new-symbol direction next-state))
-(defparameter *transitions*
-  '(((start 1)   (0 right start))
-    ((loop 0)    (1 left  start))
-    ((start 0)   (0 right accept))
-    ((loop 1)    (1 right accept))))
 
-(defun process-step (state tape)
+
+(defun process-step (state tape transitions)
   (let* ((symbol (tape-current tape))
-         (rule (assoc (list state symbol) *transitions* :test #'equal)))
+         (rule (gethash (list state symbol) transitions)))
     (if rule
-        (destructuring-bind (new-symbol direction next-state) (second rule)
-          ;; Construct a new tape with the current symbol changed
+        (destructuring-bind (new-symbol direction next-state) rule
           (let ((updated-tape (make-tape
                                 :left (tape-left tape)
                                 :current new-symbol
                                 :right (tape-right tape))))
-            ;; Move based on direction
             (let ((moved-tape
                     (cond ((eq direction 'left)  (move-left updated-tape))
                           ((eq direction 'right) (move-right updated-tape))
                           (t updated-tape))))
               (values next-state moved-tape))))
-        ;; No rule found: halt
         (values nil tape))))
 
-(defun run-machine (state tape)
-  (loop 
+(defun run-machine (initial-state initial-tape transitions)
+  (loop with state = initial-state
+        with tape = initial-tape
     do (format t "~%State: ~a" state)
     (print-tape tape)
-    (multiple-value-bind (next-state new-tape) (process-step state tape)
-    (if next-state
-      (progn (setf state next-state)
-              (setf tape new-tape))
-      (return (values state tape))))))
+    (multiple-value-bind (next-state new-tape) (process-step state tape transitions)
+      (if next-state
+          (progn (setf state next-state)
+                 (setf tape new-tape))
+          (return (values state tape))))))
 
+
+(defmacro define-turing-machine (name &body transitions)
+  `(defparameter ,(intern (format nil "*~a-TRANSITIONS*" (string-upcase name)))
+     (let ((table (make-hash-table :test #'equal)))
+       (dolist (transition ',transitions table)
+         (destructuring-bind (current-state current-symbol next-symbol direction next-state) transition
+           (setf (gethash (list current-state current-symbol) table)
+                 (list next-symbol direction next-state)))))))
+
+
+
+(define-turing-machine simple-adder
+  (start 1 1 right start)
+  (start 0 0 right start)
+  (start _ _ left carry)
+  (carry 0 1 right final)
+  (carry _ 1 right  final)
+  (carry 1 0 left carry))
+
+(define-turing-machine another-machine
+  (q1 a b right q2)
+  (q2 b a left  q1)
+  (q2 _ _ right halt))
 
 (defun main ()
-  (let ((initial-tape (make-tape :left '(0 0) :current 1 :right '(0 1 0 1 0))))
-    (multiple-value-bind (final-state final-tape) (run-machine 'start initial-tape)
-      (format t "~%Final state: ~a" final-state)
+  (let ((initial-tape (make-tape :left '(0 0) :current 1 :right '(0 1 0 1 1))))
+    (multiple-value-bind (final-state final-tape)
+        (run-machine 'start initial-tape *simple-adder-transitions*)
+      (format t "~%Final state (simple-adder): ~a" final-state)
       (print-tape final-tape))))
